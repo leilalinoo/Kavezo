871fc7514026559efce4ca32595ecd61bb438504
PappNiki20 <papp.nikoletta001@gmail.com>
2023-11-20 11:53:34 +0100

    kaveview elkezdve

--- src/nezet/KaveView.java
+++ src/nezet/KaveView.java
@@ -1,7 +1,25 @@
 
 package nezet;
 
+import java.awt.BorderLayout;
+import java.util.Scanner;
+import modell.Kavezo;
+
 
 public class KaveView {
+    
+    public void KaveView(){
+      String ital = rendelesFelvesz();
+    
+    }
+    Kavezo Starbucks = new Kavezo(5,4,3);
+    Scanner sc = new Scanner(System.in);
+
+    
+    public String rendelesFelvesz(){
+    System.out.println("Mit szeretne inni? {Kave, Tea, Forrocsoki}");
+    String  valasztas = sc.nextLine();
+    return valasztas;
+    }
     
 }
